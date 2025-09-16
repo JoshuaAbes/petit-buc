45977665aca64fa309ebaf9ac521850fa5ef675d
JoshuaAbes <joshua.abessolo@heig-vd.ch>
2025-09-16 16:55:43 +0200

    Replace Tailwind CSS plugin with Vue plugin in Vite configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const App = {
+    template: `<div style='font-size:2rem;text-align:center;margin-top:2rem;'>Petit Buc Frontend OK</div>`
+};
+
+createApp(App).mount('#app');
